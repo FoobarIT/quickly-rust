8e6d9515053d814eb7636aa635cdf1d2227bda9f
FoobarIT <55385221+FoobarIT@users.noreply.github.com>
2024-10-06 23:50:27 +0200

    refactor: Middleware system

--- src/quickly/app.rs
+++ src/quickly/app.rs
@@ -2,11 +2,16 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use crate::quickly::http::{Request, Response}; // On importe les nouveaux types Request et Response
 use crate::quickly::router::Router;
-use crate::quickly::middleware::Middleware;
-
 
 const ADDR: &str = "127.0.0.1";
 const BUFFER_SIZE: usize = 1024;
+
+
+struct Middleware {
+    path: Option<String>,
+    func: Box<dyn Fn(&mut Request, &dyn Fn(&mut Request) -> Response) -> Response>,
+}
+
 pub struct App {
     router: Router,
     middlewares: Vec<Middleware>,
@@ -41,12 +46,18 @@ impl App {
     pub fn head(&mut self, path: &str, handler: fn(&mut Request, Response) -> Response) {
         self.router.add_route("HEAD", path, handler);
     }
-    pub fn use_middleware(&mut self, middleware: Middleware) {
-        self.middlewares.push(middleware);
+    pub fn work<F>(&mut self, path: Option<&str>, func: F)
+    where 
+        F: Fn(&mut Request, &dyn Fn(&mut Request) -> Response) -> Response + 'static,
+    {
+        self.middlewares.push(Middleware {
+            path: path.map(|p| p.to_string()),
+            func: Box::new(func),
+        });
     }
 
     pub fn run(&mut self, port: &str) {
-        let addr = format!("{} {}",ADDR, port);
+        let addr = format!("{}:{}",ADDR, port);
         println!("Listening on http://{}", addr);
 
         let listener = TcpListener::bind(&addr).expect("Failed to bind to address");
@@ -93,84 +104,19 @@ impl App {
         let mut next: Box<dyn Fn(&mut Request) -> Response> = Box::new(|req: &mut Request| router.handle_request(req));
 
         for middleware in self.middlewares.iter().rev() {
-            let old_next = next; // Capture l'ancien "next"
-            next = Box::new(move |req: &mut Request| middleware(req, &old_next));
+            let old_next = next;
+            let path = middleware.path.clone();
+            next = Box::new(move |req: &mut Request| {
+                if let Some(ref p) = path {
+                    if req.path.starts_with(p) {
+                        return (middleware.func)(req, &old_next);
+                    }
+                } else {
+                    return (middleware.func)(req, &old_next);
+                }
+                old_next(req)
+            });
         }
-
-        // Exécution du premier middleware ou de la route
         next(req)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn mock_handler(_req: &mut Request, _res: Response) -> Response {
-        Response::new(200, "Ok")
-    }
-
-    fn test_route(app: &mut App, method: &str, path: &str) {
-        match method {
-            "GET" => app.get(path, mock_handler),
-            "POST" => app.post(path, mock_handler),
-            "PUT" => app.put(path, mock_handler),
-            "DELETE" => app.delete(path, mock_handler),
-            "PATCH" => app.patch(path, mock_handler),
-            "OPTIONS" => app.options(path, mock_handler),
-            "HEAD" => app.head(path, mock_handler),
-            _ => panic!("Unsupported method"),
-        }
-        app.router.add_route(method, path, mock_handler);
-    }
-
-    #[test]
-    fn test_get_route() {
-        let mut app = App::new();
-        test_route(&mut app, "GET", "/test");
-    }
-
-    #[test]
-    fn test_post_route() {
-        let mut app = App::new();
-        test_route(&mut app, "POST", "/test");
-    }
-
-    #[test]
-    fn test_put_route() {
-        let mut app = App::new();
-        test_route(&mut app, "PUT", "/test");
-    }
-
-    #[test]
-    fn test_delete_route() {
-        let mut app = App::new();
-        test_route(&mut app, "DELETE", "/test");
-    }
-
-    #[test]
-    fn test_patch_route() {
-        let mut app = App::new();
-        test_route(&mut app, "PATCH", "/test");
-    }
-
-    #[test]
-    fn test_options_route() {
-        let mut app = App::new();
-        test_route(&mut app, "OPTIONS", "/test");
-    }
-
-    #[test]
-    fn test_head_route() {
-        let mut app = App::new();
-        test_route(&mut app, "HEAD", "/test");
-    }
-
-    #[test]
-    fn test_use_middleware() {
-        let mut app = App::new();
-        let middleware: Middleware = |req, next| next(req);
-        app.use_middleware(middleware);
-        assert_eq!(app.middlewares.len(), 1);
     }
 }
